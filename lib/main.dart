8e570ef7c1a483de1a73aeea77dd61c64048d2f6
Pratik Thombare <96099806+pratikt76@users.noreply.github.com>
2022-06-20 14:35:32 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:day11_weather_app/data_service.dart';
+import 'package:flutter/material.dart';
+
+import 'models.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _cityTextController = TextEditingController();
+  final _dataService = DataService();
+
+  WeatherResponse _response;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_response != null)
+              Column(
+                children: [
+                  Image.network(_response.iconUrl),
+                  Text(
+                    '${_response.tempInfo.temperature}°',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  Text(_response.weatherInfo.description)
+                ],
+              ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: SizedBox(
+                width: 150,
+                child: TextField(
+                    controller: _cityTextController,
+                    decoration: InputDecoration(labelText: 'City'),
+                    textAlign: TextAlign.center),
+              ),
+            ),
+            ElevatedButton(onPressed: _search, child: Text('Search'))
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    setState(() => _response = response);
+  }
+}
